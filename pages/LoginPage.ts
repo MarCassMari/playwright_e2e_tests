9268a59cf92206168d38b7eb5b204f754c2a81bf
MarCassMari <marcassmachado32@gmail.com>
2026-02-02 18:54:43 -0300

    feat - massa de dados e cenários de login

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,19 +5,21 @@ export class LoginPage {
   readonly usernameInput: Locator;
   readonly passwordInput: Locator;
   readonly loginButton: Locator;
+  readonly errorMessage: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.usernameInput = page.locator('[data-test="username"]');
     this.passwordInput = page.locator('[data-test="password"]');
     this.loginButton = page.locator('[data-test="login-button"]');
+    this.errorMessage = page.locator('[data-test="error"]');
   }
 
-  async goto() {
-    await this.page.goto('https://www.saucedemo.com/');
+  async acessarPagina() {
+    await this.page.goto('/');
   }
 
-  async login(user: string, pass: string) {
+  async realizarLogin(user: string, pass: string) {
     await this.usernameInput.fill(user);
     await this.passwordInput.fill(pass);
     await this.loginButton.click();
